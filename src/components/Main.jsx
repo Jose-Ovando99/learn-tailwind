84dfba34475e3021bd79b3680e2202a5b9f8423d
Jose-Ovando99 <162391184+Jose-Ovando99@users.noreply.github.com>
2024-04-28 08:38:42 -0600

    he aprendido lo basico de tailwind con esta sencilla card

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
+import perfil1 from '../assets/perfil-1.png';
 
-function Main({ alt, title, caption, autor, fecha  }) {
+function Main({ alt, title, caption, autor, fecha }) {
   return (
     <main className='p-4'>
-      <article>
-        <div><img src='https://ui-avatars.com/api?name=Ivan+Ovando' alt={alt} /></div>
-        <div>
-          <h3>{title}</h3>
-          <h2>{caption}</h2>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam, velit praesentium? Et modi voluptate non explicabo quasi! Quibusdam molestias error iure incidunt veniam unde corporis earum mollitia reprehenderit? Harum, quod!</p>
+      <article className='overflow-hidden rounded bg-white shadow'>
+        <div className='h-52'>
+          <img 
+          className='h-full w-full object-contain'
+          src={perfil1} alt={alt} />
+          </div>
+        <div className='p-5 space-y-3'>
+          <h3 className='text-sm font-semibold text-sky-500'>{title}</h3>
+          <h2 className='text-xl font-semibold text-slate-800 leading-tight'>{caption}</h2>
+          <p className='text-slate-500'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam, velit praesentium? Et modi voluptate non explicabo quasi! Quibusdam molestias error iure incidunt veniam unde corporis earum mollitia reprehenderit? Harum, quod!</p>
         </div>
-        <div>
-          <img src={'https://ui-avatars.com/api?name=' + autor} alt={autor} />
-          <div>
-            <span>{autor}</span>
-            <span>{fecha}</span>
+        <div className='flex space-x-2 p-5'>
+          <img className='h-10 w-10 rounded-full' src={'https://ui-avatars.com/api?name=' + autor} alt={autor} />
+          <div className='flex flex-col justify-center'>
+            <span className='text-sm font-semibold leading-4 text-slate-600'>{autor}</span>
+            <span className='text-sm text-slate-500'>{fecha}</span>
           </div>
         </div>
       </article>
